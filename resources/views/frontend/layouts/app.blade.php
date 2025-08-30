ed53ecc4a076df91c3c7d68af45e71dbae01222d
pentanikit <pentanikit2023@gmail.com>
2025-08-30 13:32:35 +0600

    new update with title and email markdown fixed

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -23,7 +23,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title>{{ $title ?? 'Home' }}</title>
+  <title>{{ $title ?? config('app.name') }}</title>
   <meta name="language" content="English">
   <meta name="author" content="{{ config('app.name') }}">
   <meta name="csrf-token" content="{{ csrf_token() }}">
